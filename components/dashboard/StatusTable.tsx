875324a113544530956dbd07d4afc58a0cce3d63
JakubKw19 <kkwiecie@interia.pl>
2025-05-31 22:37:57 +0200

    refactor: clean up imports and improve component structure in dashboard

--- components/dashboard/StatusTable.tsx
+++ components/dashboard/StatusTable.tsx
@@ -1,8 +1,7 @@
 'use client';
-import React from "react";
-import { Badge } from "../ui/badge";
-import { Card, CardDescription, CardFooter, CardHeader, CardTitle } from "../ui/card";
-
+import React from 'react';
+import { Badge } from '../ui/badge';
+import { Card, CardDescription } from '../ui/card';
 
 interface StatusTableProps {
   data: {
@@ -17,13 +16,15 @@ export default function StatusTable({ data }: StatusTableProps) {
   return (
     <div className="grid grid-cols-4 gap-3 p-3">
       {panelStatuses.map((panel) => (
-        <Card key={panel.id} className="flex flex-col items-center justify-center p-2 text-xs hover:cursor-pointer hover:bg-green-700" onClick={() => {
-          handleSetCurrentPanel(panel.id);
-        }}>
-          <CardDescription className="text-center">
-            #{panel.id}
-          </CardDescription>
-          <Badge variant={panel.status === "online" ? "default" : "destructive"}>
+        <Card
+          key={panel.id}
+          className="flex flex-col items-center justify-center p-2 text-xs hover:cursor-pointer hover:bg-green-700"
+          onClick={() => {
+            handleSetCurrentPanel(panel.id);
+          }}
+        >
+          <CardDescription className="text-center">#{panel.id}</CardDescription>
+          <Badge variant={panel.status === 'online' ? 'default' : 'destructive'}>
             {panel.status}
           </Badge>
         </Card>
@@ -31,4 +32,3 @@ export default function StatusTable({ data }: StatusTableProps) {
     </div>
   );
 }
-
